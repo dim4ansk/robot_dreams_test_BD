e19d22197075ea7cee39d6d321afc47ed40e8b9c
levoy <levoyflang@gmail.com>
2023-03-05 22:54:00 +0300

    add multiply

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 def func(a, b):
     return f" Your result {a+b}"
 
+def func_2(x, y):
+    return x*y
+
 
 if __name__ == "__main__":
     print(func(int(input("Enter your first number: ")), int(input("Enter your second number: "))))
+    print(func_2(2, 10))
