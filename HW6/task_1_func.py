099f4bbb1e6fc8bea94b60182673a472d3e5822d
levoy <levoyflang@gmail.com>
2023-03-25 15:51:46 +0300

    HW6

---
+++ HW6/task_1_func.py
@@ -0,0 +1,16 @@
+# ===============================================  TASK 1 =============================================
+
+def exponentiation(num, st):
+    return num**st
+
+
+print(exponentiation(float(input("Enter num: ")), float(input("Enter power value: "))))
+
+
+# ===============================================  TASK 2 =============================================
+
+def super_sum(*args):
+    return sum(args)
+
+
+print(super_sum(45, 6, 23, -5.6, 7, 0))
