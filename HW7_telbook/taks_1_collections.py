112a19ab3ec593996fd2a1cbc6d511e3e8135a46
levoy <levoyflang@gmail.com>
2023-03-27 21:33:25 +0300

    HW7

---
+++ HW7_telbook/taks_1_collections.py
@@ -0,0 +1,91 @@
+
+print("Hi. This is a phone book. \nIf you have a new friend and you want to communicate with him, "
+      "you can save his contact. "
+      "\nFollow the menu instructions and make corrections to the book. GLHF!\n")
+
+print("1 - Add contact\n"
+      "2 - Number of records\n"
+      "3 - A list of all the names in the book\n"
+      "4 - Contact's detailed information\n"
+      "5 - Delete contact\n"
+      "0 - Exit the program")
+
+
+dict_contact = {}
+
+
+def add():
+    while True:
+        name = input("Enter the person's name: ")
+        number = input("Enter the person's number: ")
+        if len(name) > 0 and number.isdigit() and len(number) == 10:
+            if name in [i for i in dict_contact.keys()]:
+                print("A contact with this name is already in your book!")
+                break
+            elif number in [i for i in dict_contact.values()]:
+                print("A contact with this number is already in your book!")
+                break
+            else:
+                dict_contact[name] = number
+                print("Done!")
+                break
+        else:
+            print("The name or number entered is incorrect. Try again!\n")
+
+
+def stats():
+    if len(dict_contact) == 0:
+        print('List is empty! Please add contacts!')
+    else:
+        print(f"You have {len(dict_contact)} contacts.")
+
+
+def all_name_contacts():
+    lst_na = [i for i in dict_contact.keys()]
+    if len(lst_na) == 0:
+        print('List is empty! Please add contacts!')
+    else:
+        for i, name in enumerate(lst_na):
+            print(f"{i+1} - {name}")
+
+
+def info():
+    name_info = input("Enter name by which you want to find information: ")
+    if name_info in [i for i in dict_contact.keys()]:
+        print(f"Phone number of {name_info} - {dict_contact.get(name_info)}")
+    else:
+        print("No such contact exist. Enter another name or add to phone book!")
+
+
+def delete():
+    name_del = input("Enter name by which you want to delete contact: ")
+    if name_del in [i for i in dict_contact.keys()]:
+        dict_contact.pop(name_del)
+        print(f"Contact {name_del} has been removed from the book")
+    else:
+        print("No such contact exist. Enter another name or add to phone book!")
+
+
+def menu(num):
+    if num == 1:
+        add()
+    elif num == 2:
+        stats()
+    elif num == 3:
+        all_name_contacts()
+    elif num == 4:
+        info()
+    elif num == 5:
+        delete()
+
+
+while True:
+    choice = input("\nSelect a command by entering a number: ")
+    print()
+    if choice.isdigit() and 6 > int(choice) > 0:
+        menu(int(choice))
+    elif choice == "0":
+        print("Bye!")
+        break
+    else:
+        print("You can enter only numbers whose value is from 1 to 5 include or 0 for exit. Try again!")
